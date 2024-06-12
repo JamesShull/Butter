628d1c5731122e45087eb69a3dac014cdc562a85
James <jshull@hme.com>
2024-06-12 00:47:38 -0700

    add state class

---
+++ app/state.py
@@ -0,0 +1,48 @@
+class State():
+    '''State can be used to push entire application state to GUI.'''
+    def __init__(self):
+        self.defaults = {
+            'config': None,
+            'status': 'Ready'
+        }
+        self.resetToDefaults()
+
+    def set(self, key, value):
+        """
+        Set a value in the cache.
+        Args:
+            key (str): The key for the value.
+            value: The value to be stored.
+        """
+        if key in self.cache:
+            self.cache[key] = value
+    
+    def get(self, key):
+        """
+        Get a value from the cache.
+        Args:
+            key (str): The key for the value.
+        Returns:
+            The value associated with the key, or None if not found.
+        """
+        if key in self.cache:
+            return self.cache[key]
+    
+    def delete(self, key):
+        """
+        Delete a value from the cache.
+        Args:
+            key (str): The key for the value.
+        """
+        if key in self.cache:
+            del self.cache[key]
+
+    def clear(self):
+        """
+        Clear the entire cache.
+        """
+        self.cache.clear()
+    
+    def resetToDefaults(self):
+        """Reset cache to default values."""
+        self.cache = self.defaults
